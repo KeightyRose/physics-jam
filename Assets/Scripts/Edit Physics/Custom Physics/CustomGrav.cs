6f877d85d5f681997491319e2e1d8f807c80905f
Tumi Jourdan <2180153@students.wits.ac.za>
2023-08-23 19:41:19 +0200

    Added custom gravity

---
+++ Assets/Scripts/Edit Physics/Custom Physics/CustomGrav.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGrav : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rb2d;
+    private Vector2 gravityForce = Vector2.zero;
+    void Start()
+    {
+        rb2d= GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2d.AddForce(gravityForce * rb2d.mass);
+    }
+
+    private void changeGravity(Vector2 gravIn)
+    {
+       gravityForce = gravIn;
+    }
+}
